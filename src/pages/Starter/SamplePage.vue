35858b04006dbf2c23075d3fdd7eff54bcc2e680
JuanGallooo <juanesteban3600@gmail.com>
2019-11-26 19:07:01 -0500

    Table , and graphical method

--- src/pages/Starter/SamplePage.vue
+++ src/pages/Starter/SamplePage.vue
@@ -1,12 +1,30 @@
 <template>
-  <div class="row"><h3>Starter page</h3></div>
+  <div class="container">
+    <div class="row">
+      <h3>Starter page</h3>
+    </div>
+    <div class="d-flex justify-content-center">
+      <app-table></app-table>
+    </div>
+  </div>
 </template>
 <script>
+import graphical from '../../graphicalMethod.vue'
 export default {
   name: 'starter-page',
+  
   data() {
-    return {};
+    return {
+      
+    };
+  },
+  components: {
+      'app-table' : graphical
+  },
+  methods: {
+
   }
 };
+
 </script>
 <style></style>
