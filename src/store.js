fef35acb9f2c9498bb5aaec7e274127bf3a398c3
Christian Gallo <cgp9923@gmail.com>
2020-11-18 19:10:45 -0500

    update store.j

--- src/store.js
+++ src/store.js
@@ -5,9 +5,9 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        //backend: process.env.NODE_ENV=='production' ? 'https://icesi-vip-alien-backend.herokuapp.com/api' : 'http://localhost:8080/api'
-        backend: 'http://localhost:8080/api',
-        token: '',
+        backend: process.env.NODE_ENV=='production' ? 'https://icesi-vip-alien-backend.herokuapp.com/api' : 'http://localhost:8080/api'
+        /* backend: 'http://localhost:8080/api',
+        token: '', */
     }
 })
 
