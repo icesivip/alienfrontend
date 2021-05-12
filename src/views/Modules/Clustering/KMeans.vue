b9196d7fbcb4ce9215d6bdcc82dfd70c1421858b
Zonitidus <zonitidus@gmail.com>
2021-05-12 18:18:44 -0500

    add file upload feat

--- src/views/Modules/Clustering/KMeans.vue
+++ src/views/Modules/Clustering/KMeans.vue
@@ -2,29 +2,61 @@
     <div>
         <card>
             <h2>Select your file</h2>
-            <div>
-                <label for="clusteringFile" class="my-auto">Choose your file</label>
-                <el-input type="file" outlined id="clusteringFile"></el-input>
-                <base-button>Send</base-button> 
+            <div class="container">
+                <div class="large-12 medium-12 small-12 cell">
+                    <label>File
+                    <input type="file" id="file" ref="file" v-on:change="handleFileUpload()"/>
+                    </label>
+                    <button v-on:click="submitFile()">Submit</button>
+                </div>
             </div>
         </card>
     </div>
+
 </template>
 
 <script>
 export default {
-    name:"km",
+
+    name: "Km",
+
     data(){
 
         return{
-
+            file: ''
         }
+
     },
 
     methods: {
+        handleFileUpload(){
+            this.file.$refs.file.files[0];
+        },
 
+    
+        async submitFile(){
+
+            let formData = new FormData();
+
+            formData.append('file', this.file);
+
+            axios.post( '/single-file',
+                formData,
+                {
+                    headers: {
+                    'Content-Type': 'multipart/form-data'
+                    }
+                }
+            ).then(function(){
+                console.log('SUCCESS!!');
+            })
+            .catch(function(){
+                console.log('FAILURE!!');
+            });
+        }
     }
-}
+  }
+
 </script>
 
 <style>
