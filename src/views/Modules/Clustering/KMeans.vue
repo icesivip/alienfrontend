6b036d0deb381bb192d232c5d908dcd0d545604b
Zonitidus <zonitidus@gmail.com>
2021-05-26 20:02:56 -0500

    Add send files function.

--- src/views/Modules/Clustering/KMeans.vue
+++ src/views/Modules/Clustering/KMeans.vue
@@ -1,4 +1,6 @@
 <template>
+
+    
     <div>
         <card>
             <h2>Select your file</h2>
@@ -10,12 +12,23 @@
                     <button v-on:click="submitFile()">Submit</button>
                 </div>
             </div>
+
+            <button v-on:click="get_test()">TEST</button>
+
+
+
+            <div id="plotlyExample" style="grid-template-columns: 50% 50%; display: grid; grid-template-rows: 400px 100px">
+                <plotly-graph @hover="hover" v-bind:data="scatterPlotData" div-id="plot2" v-bind:height="90" :column-number="2"></plotly-graph>
+                <input type="range" min="-10" max="10" v-model="currentVal" @change="sliderUpdate" style="grid-column-start: 1; grid-column-end: 3; width: 50%; margin-left: auto; margin-right: auto" />
+            </div>
         </card>
     </div>
-
 </template>
 
 <script>
+import axios from 'axios';
+
+
 export default {
 
     name: "Km",
@@ -23,37 +36,63 @@ export default {
     data(){
 
         return{
-            file: ''
+            file: '',
+
+            graph: ''
         }
 
     },
+    mounted() {
+      let recaptchaScript = document.createElement('script')
+      recaptchaScript.setAttribute('src', 'https://cdn.plot.ly/plotly-latest.min.js')
+      document.head.appendChild(recaptchaScript)
+    },
 
     methods: {
-        handleFileUpload(){
-            this.file.$refs.file.files[0];
-        },
-
-    
-        async submitFile(){
+        
+        submitFile(){
 
             let formData = new FormData();
 
             formData.append('file', this.file);
 
-            axios.post( '/single-file',
+            axios.post( 'http://localhost:5000/upl',
                 formData,
                 {
                     headers: {
                     'Content-Type': 'multipart/form-data'
                     }
                 }
-            ).then(function(){
+            ).then(function (response){
                 console.log('SUCCESS!!');
+
+                console.log(response.data);
+
+
+                //this.graph = Plotly.newPlot('plotlyExample-div', response, response.data.layout);
+
+                this.graph = response.data;
+
             })
             .catch(function(){
                 console.log('FAILURE!!');
             });
+        },
+
+        handleFileUpload(){
+            this.file = this.$refs.file.files[0];
+        },
+
+        get_test(){
+            axios.get('http://localhost:5000/testt')
+            .then(response => {
+                console.log(response.data);})
+            
+            .catch(e => {
+                console.log(e);
+            })
         }
+
     }
   }
 
