4dd536ebdc9cbdf5b2b4eed41056a2f51c6e30b7
Zonitidus <zonitidus@gmail.com>
2021-06-01 15:46:28 -0500

    Added kmeans graph (still needs to be set dynamically)

--- src/views/Modules/Clustering/KMeans.vue
+++ src/views/Modules/Clustering/KMeans.vue
@@ -15,7 +15,11 @@
 
             <button v-on:click="get_test()">TEST</button>
 
+            <h1>{{ xd }}</h1>
 
+            <div class="scat" style="width: 80%">
+                <canvas id="myChart"></canvas>
+            </div>
 
             
         </card>
@@ -35,14 +39,17 @@ export default {
         return{
             file: '',
 
-            graph: ''
+            scatter: '',
+
+            s1: '',
+            
+            s2: '',
+
+            s3: '',  
+
+            xd: ''
         }
 
-    },
-    mounted() {
-      let recaptchaScript = document.createElement('script')
-      recaptchaScript.setAttribute('src', 'https://cdn.plot.ly/plotly-latest.min.js')
-      document.head.appendChild(recaptchaScript)
     },
 
     methods: {
@@ -60,19 +67,20 @@ export default {
                     'Content-Type': 'multipart/form-data'
                     }
                 }
-            ).then(function (response){
+            ).then((response) => {
                 console.log('SUCCESS!!');
 
                 console.log(response.data);
 
+                this.scatter = response.data;
 
-                //this.graph = Plotly.newPlot('plotlyExample-div', response, response.data.layout);
+                console.log(Object.keys(this.scatter).length);
 
-                this.graph = response.data;
+                this.graphRoute();
 
             })
-            .catch(function(){
-                console.log('FAILURE!!');
+            .catch(function(response){
+                console.log(response.message);
             });
         },
 
@@ -83,11 +91,92 @@ export default {
         get_test(){
             axios.get('http://localhost:5000/testt')
             .then(response => {
-                console.log(response.data);})
+                console.log(response.data);
+                this.xd = 'testeado';})
             
             .catch(e => {
                 console.log(e);
             })
+        },
+
+        graphRoute(){
+
+            this.formatData();
+
+            var ctx = document.getElementById('myChart').getContext('2d');
+            var scatterChart = new Chart(ctx, {
+                type: 'scatter',
+                data: {
+                    datasets: [{
+                        label: 'K1',
+                        lineTension: 0,
+                        showLine:false,
+                        fill: false,
+                        borderColor: 'rgba(0, 113, 206, 1)', 
+                        data: this.s1
+                    },{
+
+                        label: 'k2',
+                        lineTension: 0,
+                        showLine:false,
+                        fill: false,
+                        borderColor: 'rgba(241, 74, 38, 1)', 
+                        data: this.s2
+                    
+                    },{
+
+                        label: 'k3',
+                        lineTension: 0,
+                        showLine:false,
+                        fill: false,
+                        borderColor: 'rgba(72, 241, 38, 1)', 
+                        data: this.s3
+                    
+                    }],
+                    
+                },
+                options: {
+                    scales: {
+                        xAxes: [{
+                            type: 'linear',
+                            position: 'bottom',
+                            offset: true
+                        }]
+                    },
+                }
+            });
+        },
+
+        formatData(){
+            
+
+            let data = [];
+
+
+            for(let i=0; i < Object.keys(this.scatter).length; i++){
+
+                let dic = []; 
+
+                for(let j=0; j < this.scatter[i].length; j++){
+                    
+                    let tempDic = {};
+
+                    tempDic['x'] = this.scatter[i][j][0];
+                    tempDic['y'] = this.scatter[i][j][1];
+
+                    dic.push(tempDic);
+                }
+
+                data.push(dic);
+            }
+
+           
+
+            this.scatter = data;
+            this.s1 = data[0];
+            this.s2 = data[1];
+            this.s3 = data[2];
+
         }
 
     }
